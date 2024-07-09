123e376d952079a06f369784e50d42908e83a375
manikanta-gbv <gbvmanikanta13@gmail.com>
2024-07-09 23:52:27 +0530

    inserting llm_configuration.py

---
+++ utils/llm_configuration.py
@@ -0,0 +1,11 @@
+import os
+from datetime import datetime
+
+from langchain.chat_models import ChatOpenAI
+import config
+
+llm_model = "gpt-3.5-turbo"
+temperature = 0.5
+benchmark_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.%f")
+
+llm = ChatOpenAI(model=llm_model, openai_api_key=config.OPENAI_API_KEY, temperature=0.5, top_p=0.9)
